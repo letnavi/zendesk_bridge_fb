49862691a4a6a443fc52d47095e5ef63db2742dd
Ivan Televniy <filondesign@gmail.com>
2018-10-29 04:02:08 +0300

    base api

--- main.go
+++ main.go
@@ -1,36 +1,74 @@
 package main
 
 import (
-	"log"
-	"net/http"
-
+	"encoding/json"
 	"github.com/MEDIGO/go-zendesk/zendesk"
 	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
 )
 
-type Configuration struct {
-	ZendeskDomain   []string
-	ZendeskUsername []string
-	ZendeskPassword []string
-}
+// port for server
+const port = ":9090"
 
-const jsonData = "resources/create.json"
-
+// Processing facebook and zendesk requests
 func main() {
 
-	r := gin.Default()
+	// zendesk client
+	// sub-domain, email/login and password
+	client, err := zendesk.NewClient("", "", "")
+
+	// if not connect
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// gin route
+	route := gin.Default()
+	r := route.Group("/api/v1")
 	r.Use(LiberalCORS)
+
+	// test ping pong
 	r.GET("/ping", pong)
-	r.POST("/create", create)
-	r.Run(":9090")
+
+	//------------------------------------------------------------------------------------------------------------------
+	// FACEBOOK
+	//------------------------------------------------------------------------------------------------------------------
+
+	// add comment for facebook
+	r.POST("/fb/comment", func(c *gin.Context) {
+		createWorkplaceComment(c, client)
+	})
+
+	//------------------------------------------------------------------------------------------------------------------
+	// ZENDESK
+	//------------------------------------------------------------------------------------------------------------------
+
+	// add ticket
+	r.POST("/ticket", func(c *gin.Context) {
+		createTicket(c, client)
+	})
+
+	// update ticket and/or add comment
+	r.PUT("/ticket", func(c *gin.Context) {
+		updateTicket(c, client)
+	})
+	r.Run(port)
 }
 
+func createWorkplaceComment(c *gin.Context, client zendesk.Client) {
+
+}
+
+// Test func
+// Ping Pong
 func pong(c *gin.Context) {
 	c.JSON(200, gin.H{
 		"message": "pong",
 	})
 }
 
+//
 func LiberalCORS(c *gin.Context) {
 	c.Header("Access-Control-Allow-Origin", "*")
 	if c.Request.Method == "OPTIONS" {
@@ -41,16 +79,58 @@ func LiberalCORS(c *gin.Context) {
 	}
 }
 
-func create(c *gin.Context) {
+// Create ticket, see doc
+// https://developer.zendesk.com/rest_api/docs/core/tickets#create-ticket
+//
+// Create ticket with a set of parameters.
+// Create comments if there is one in json
+// To understand the features, see zendesk.Ticket obj
+func createTicket(c *gin.Context, z zendesk.Client) {
 
-	// Коннескт
-	client, err := zendesk.NewClient("", "", "")
+	ticketData := &zendesk.Ticket{}
+	body, err := c.GetRawData()
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	json.Unmarshal(body, &ticketData)
+
+	_, err = z.CreateTicket(ticketData)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	c.JSON(200, gin.H{
-		"status": action.createAction(jsonData, client),
+		"status": "ok",
+	})
+}
+
+// Update ticket, see doc
+// https://developer.zendesk.com/rest_api/docs/core/tickets#update-ticket
+//
+// Ticket update
+// Add and update comment
+// Depending on json content
+func updateTicket(c *gin.Context, z zendesk.Client) {
+
+	t := &zendesk.Ticket{}
+	body, err := c.GetRawData()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	json.Unmarshal(body, &t)
+
+	id := t.ID
+
+	_, err = z.UpdateTicket(id, t)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	c.JSON(200, gin.H{
+		"status": "ok",
 	})
 }
